5eb1e23db9eff3d6ed1b3b99f6128b759e0a8e61
Povilas Balciunas <balciunas90@gmail.com>
2018-10-24 23:49:07 +0300

    feat/libutp: compile libutp with rust build script
    This is supposed to be more portable than using makefile.

--- libutp-sys/build.rs
+++ libutp-sys/build.rs
@@ -1,11 +1,50 @@
 extern crate bindgen;
+#[macro_use]
+extern crate unwrap;
+extern crate cc;
 
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
 fn main() {
-    println!("cargo:rustc-link-lib=utp");
+    if !Path::new("curl/.git").exists() {
+        let _ = Command::new("git")
+            .args(&["submodule", "update", "--init", "--recursive"])
+            .status();
+    }
 
+    let out_dir = PathBuf::from(unwrap!(env::var("OUT_DIR")));
+    compile_libutp(&out_dir);
+    println!("cargo:rustc-link-lib=static=utp");
+    println!("cargo:rustc-link-search=native=build");
+    gen_libutp_rust_bindings(&out_dir);
+}
+
+fn compile_libutp(out_dir: &PathBuf) {
+    let target = env::var("TARGET").unwrap();
+    let windows = target.contains("windows");
+
+    let mut cfg = cc::Build::new();
+    cfg.out_dir(out_dir)
+        .cpp(true)
+        .include("libutp")
+        .file("libutp/utp_api.cpp")
+        .file("libutp/utp_callbacks.cpp")
+        .file("libutp/utp_hash.cpp")
+        .file("libutp/utp_internal.cpp")
+        .file("libutp/utp_packedsockaddr.cpp")
+        .file("libutp/utp_utils.cpp");
+    if windows {
+        cfg.file("libutp/libutp_inet_ntop.cpp");
+    } else {
+        cfg.define("POSIX", None);
+    }
+    cfg.compile("utp");
+}
+
+/// Write the bindings to the $OUT_DIR/bindings.rs file.
+fn gen_libutp_rust_bindings(out_dir: &PathBuf) {
     let bindings = bindgen::Builder::default()
         .header("utp.h")
         .layout_tests(false)
@@ -14,9 +53,7 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
+        .write_to_file(out_dir.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
